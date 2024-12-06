0a8ef85ecd8d94776000bfa2252ed3cdc104a43c
kiprotichc <kiprotichcosmas15@gmail.com>
2024-12-06 15:21:21 +0300

    defining functions in python

---
+++ functions_in_python.py
@@ -0,0 +1,39 @@
+
+# def happy_birthday(name, age):
+#     print(f"Happy Birthday, {name}! ")
+#     print(f"You are {age} years old!")
+#     print("Happy Birthday to youuuuu!")
+#     print()
+#     print()
+#     print()
+# happy_birthday("Kiprotichc", 25)
+# happy_birthday("Karonei", 18)
+# happy_birthday("Cosmas", 99)
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+
+def add(a,b):
+    z = a+b
+    return z
+def subtract(a,b):
+    z = a-b
+    return z
+def multiply(a,b):
+    z = a*b
+    return z
+
+def divide(a,b):
+    z = a/b
+    return z
+def modulo(a,b):
+    z = a%b
+    return z
+
+print(add(a,b))
+print(subtract(a,b))
+print(multiply(a,b))
+print(divide(a,b))
+print(modulo(a,b))
+
